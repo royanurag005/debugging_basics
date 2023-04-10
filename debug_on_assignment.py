45b34846a706a18b275c3169e2225b4de7cf95f6
Anurag Roy <97970838+royanurag005@users.noreply.github.com>
2023-04-10 13:26:58 +0530

    Update debug_on_assignment.py

--- debug_on_assignment.py
+++ debug_on_assignment.py
@@ -31,6 +31,8 @@ word_per_page = 0
 pages = int(input("Number of pages: "))
 # word_per_page == int(input("Number of words per page: "))
 word_per_page = int(input("Number of words per page: "))
+print(f"pages : {pages}")
+print(f"words_per_page : {word_per_page}")
 #Compares if words_per_Page is equal to the input and returns either 1 or 0
 total_words = pages * word_per_page
 print(total_words)
